484655bfaad73d22bb43932a5503398f3d126688
Abhishek Kumar <abhishek.axovel@gmail.com>
2020-02-19 17:07:26 +0530

    Create testing Log Test App

---
+++ app/src/main/java/com/shephertz/logshowlibrarytest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.shephertz.logshowlibrarytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ logcheck/src/main/java/com/shephertz/logcheck/LogDebug.java
@@ -0,0 +1,13 @@
+package com.shephertz.logcheck;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "LogDebug";
+
+    public static void checkLog(String log)
+    {
+        Log.e(TAG,log);
+    }
+}
